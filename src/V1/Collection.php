b88fc8ca59eb35fb378e372f4ac7c8dc22fd4037
Tim Nelles <tim.nelles@denkwerk.com>
2025-02-19 16:26:26 +0100

    Introduces folder support in collections

--- src/V1/Collection.php
+++ src/V1/Collection.php
@@ -13,29 +13,38 @@ declare(strict_types=1);
 namespace Opctim\BrunoLang\V1;
 
 use Opctim\BrunoLang\V1\Exception\InvalidCollectionMetaException;
+use Opctim\BrunoLang\V1\Exception\InvalidRequestsStructureException;
 use Opctim\BrunoLang\V1\Interface\WriterInterface;
 use Opctim\BrunoLang\V1\Utils\Utils;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 
+/**
+ * @phpstan-type RequestsType array<int|string, BruFile|RequestsType>
+ * @phpstan-type MetaType array{
+ *      version: string,
+ *      name: string,
+ *      type: "collection",
+ *      ignore: string[],
+ *      presets: array<string, mixed>
+ *  }
+ */
 class Collection implements WriterInterface
 {
+    /**
+     * @throws InvalidCollectionMetaException
+     */
     public function __construct(
-        /** @var array{
-         *     version: string,
-         *     name: string,
-         *     type: "collection",
-         *     ignore: string[],
-         *     presets: array<string, mixed>
-         * } $meta
-         */
+        /** @var MetaType $meta */
         private array $meta = [],
-        /** @var BruFile[] $requests */
+        /** @var RequestsType $requests */
         private array $requests = [],
         /** @var BruFile[] $environments */
         private array $environments = [],
     )
-    {}
+    {
+        self::validateMeta($meta);
+    }
 
     /**
      * @throws InvalidCollectionMetaException
@@ -49,38 +58,43 @@ class Collection implements WriterInterface
         $environments = [];
 
         foreach ($finder as $collectionFile) {
-            if ($collectionFile->getRelativePath() === '') {
+            if ($collectionFile->getRelativePath() === 'environments') {
+                // Parse environments
+                $environments[] = BruFile::parse($collectionFile->getBasename('.bru'), $collectionFile->getContents());
+            } else if ($collectionFile->getRelativePath() === '') {
+                // Parse metadata
                 if ($collectionFile->getBasename() === 'bruno.json') {
                     $meta = json_decode($collectionFile->getContents(), true);
-
-                    if (!isset($meta['name'])) {
-                        throw new InvalidCollectionMetaException("The collection bruno.json doesn't have a name.");
-                    }
-
-                    if (!isset($meta['version'])) {
-                        throw new InvalidCollectionMetaException("The collection bruno.json doesn't have a version.");
-                    }
-
-                    if ($meta['version'] !== '1') {
-                        throw new InvalidCollectionMetaException(Collection::class . ' only supports version 1.');
-                    }
                 }
 
+                // Parse bru files in directory root
                 if ($collectionFile->getExtension() === 'bru') {
                     $requests[] = BruFile::parse($collectionFile->getBasename('.bru'), $collectionFile->getContents());
                 }
-            }
+            } else {
+                // Parse nested bru files
+                $folders = explode('/', $collectionFile->getRelativePath());
 
-            if ($collectionFile->getRelativePath() === 'environments') {
-                $environments[] = BruFile::parse($collectionFile->getBasename('.bru'), $collectionFile->getContents());
+                $nested = &$requests; // Reference to the root
+
+                foreach ($folders as $folder) {
+                    $nested = &$nested[$folder]; // Going deeper
+                }
+
+                $nested[] = BruFile::parse($collectionFile->getBasename('.bru'), $collectionFile->getContents());
             }
         }
 
         return new self($meta, $requests, $environments);
     }
 
+    /**
+     * @throws InvalidRequestsStructureException
+     */
     public function write(string $filePath): static
     {
+        $filePath = rtrim($filePath, '/');
+
         // Replace trailing slash
         $filePath = preg_replace('/\/$/', '', $filePath);
 
@@ -88,21 +102,55 @@ class Collection implements WriterInterface
 
         $filesystem->mkdir($filePath);
 
-        foreach ($this->getRequests() as $bruFile) {
-            $filesystem->dumpFile($filePath . '/' . $bruFile->getName() . '.bru', $bruFile->build());
-        }
-
-        if (count($this->getEnvironments()) > 0) {
-            $filesystem->mkdir($filePath . '/environments');
-        }
+        // Recursively write requests
+        $this->writeRequests($filePath, $this->getRequests());
 
         foreach ($this->getEnvironments() as $bruFile) {
-            $filesystem->dumpFile($filePath . '/environments/' . $bruFile->getName() . '.bru', $bruFile->build());
+            $bruFile->write($filePath . '/environments');
         }
 
         $filesystem->dumpFile($filePath . '/bruno.json', Utils::jsonEncodePretty2Spaces($this->getMeta()));
 
         return $this;
+    }
+
+    /**
+     * @param string $basePath
+     * @param RequestsType $requests
+     *
+     * @throws InvalidRequestsStructureException
+     */
+    protected function writeRequests(string $basePath, array $requests): void
+    {
+        foreach ($requests as $key => $item) {
+            if (is_string($key) && is_array($item)) {
+                $this->writeRequests($basePath . '/' . $key, $item);
+            } else if (is_int($key) && $item instanceof BruFile) {
+                $item->write($basePath);
+            } else {
+                throw new InvalidRequestsStructureException('Invalid requests structure. Key "' . $key . '" and value "' . gettype($item) . '" are not supported.');
+            }
+        }
+    }
+
+    /**
+     * @param MetaType $meta
+     *
+     * @throws InvalidCollectionMetaException
+     */
+    protected static function validateMeta(array $meta): void
+    {
+        if (!isset($meta['name'])) {
+            throw new InvalidCollectionMetaException("The collection meta doesn't have a name.");
+        }
+
+        if (!isset($meta['version'])) {
+            throw new InvalidCollectionMetaException("The collection meta doesn't have a version.");
+        }
+
+        if ($meta['version'] !== '1') {
+            throw new InvalidCollectionMetaException(Collection::class . ' only supports version 1.');
+        }
     }
 
     public function getName(): string
@@ -115,23 +163,59 @@ class Collection implements WriterInterface
         return $this->meta;
     }
 
+    /**
+     * @throws InvalidCollectionMetaException
+     */
     public function setMeta(array $meta): static
     {
+        self::validateMeta($meta);
+
         $this->meta = $meta;
 
         return $this;
     }
 
+    /**
+     * @return RequestsType
+     */
     public function getRequests(): array
     {
         return $this->requests;
     }
 
+    /**
+     * @param RequestsType $requests
+     *
+     * @return $this
+     */
     public function setRequests(array $requests): static
     {
         $this->requests = $requests;
 
         return $this;
+    }
+
+    /**
+     * @return BruFile[]
+     */
+    public function getFlatRequests(): array
+    {
+        return $this->flattenRequests($this->getRequests());
+    }
+
+    protected function flattenRequests(array $input): array
+    {
+        $flattened = [];
+
+        foreach ($input as $value) {
+            if ($value instanceof BruFile) {
+                $flattened[] = $value;
+            } elseif (is_array($value)) {
+                $flattened = array_merge($flattened, $this->flattenRequests($value));
+            }
+        }
+
+        return $flattened;
     }
 
     public function getEnvironments(): array
---
+++ src/V1/Exception/InvalidRequestsStructureException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of opctim/bruno-lang
+ *
+ * (c) Tim Nelles (opctim) <kontakt@timnelles.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * information in the project composer.json or on GitHub.
+ */
+
+namespace Opctim\BrunoLang\V1\Exception;
+
+use Exception;
+
+class InvalidRequestsStructureException extends Exception
+{
+
+}
